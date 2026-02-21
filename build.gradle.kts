971ac451edda2f440579ab2012e4009f4474ce8f
AaronNath11 <aaronsuhaendi@gmail.com>
2026-02-21 23:36:00 +0700

    Add SonarCloud integration for code quality analysis

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     jacoco
     id("org.springframework.boot") version "3.5.10"
     id("io.spring.dependency-management") version "1.1.7"
+    id("org.sonarqube") version "7.1.0.6387"
 }
 
 group = "id.ac.ui.cs.advprog"
@@ -80,4 +81,11 @@ tasks.test {
 
 tasks.jacocoTestReport {
     dependsOn(tasks.test)
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "A-Aaron-Nathanael-Suhaendi-2406437073_Modul-1-Coding-Standards")
+        property("sonar.organization", "a-aaron-nathanael-suhaendi-2406437073")
+    }
 }
